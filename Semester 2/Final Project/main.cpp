21852986f447fee9e9bfcc80eedaa9c016ff62f1
Tadiwos Andargie <99669877+TadAdane@users.noreply.github.com>
2023-06-08 01:19:27 +0200

    Added Restart Button

--- Semester 2/Final Project/main.cpp
+++ Semester 2/Final Project/main.cpp
@@ -53,11 +53,10 @@ int main()
     ballSprite.setPosition(rand() % (windowWidth - ballRadius * 2) + ballRadius + 150, 10);
 
     // Blast sprite
-       sf::Texture blastTexture;
-       if (!blastTexture.loadFromFile("blast.png"))
-       {
-           return EXIT_FAILURE;
-       }
+    sf::Texture blastTexture;
+    if (!blastTexture.loadFromFile("blast.png")) {
+        return EXIT_FAILURE;
+    }
 
     sf::Sprite blastSprite(blastTexture);
     int blastFrame = 0;
@@ -66,7 +65,6 @@ int main()
     blastSprite.setTextureRect(sf::IntRect(0, 0, blastTexture.getSize().x / numBlastFrames, blastTexture.getSize().y));
     blastSprite.setOrigin(blastSprite.getTextureRect().width / 2, blastSprite.getTextureRect().height / 2);
     blastSprite.setPosition(windowWidth / 2, windowHeight / 2);
-
 
     bool ballReleased = false;
     bool passedHole = false;
@@ -104,7 +102,16 @@ int main()
     //gameOverText.setString("Game Over!");
     gameOverText.setPosition((windowWidth - gameOverText.getLocalBounds().width) / 2, windowHeight / 2);
 
+    // Restart button
+    sf::Text restartButton;
+    restartButton.setFont(font);
+    restartButton.setCharacterSize(20);
+    restartButton.setFillColor(sf::Color::White);
+    restartButton.setString("Restart");
+    restartButton.setPosition((windowWidth - restartButton.getLocalBounds().width) / 2, (windowHeight - restartButton.getLocalBounds().height) / 2 + 100);
+
     bool gameEnded = false;
+    bool restartClicked = false;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -117,105 +124,128 @@ int main()
                     ballReleased = true;
                 }
             }
+            else if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    if (restartButton.getGlobalBounds().contains(static_cast<float>(mousePosition.x), static_cast<float>(mousePosition.y))) {
+                        // Reset game variables
+                        score = 0;
+                        lives = 3;
+                        holeSpeed = 2.0f;
+                        ballReleased = false;
+                        passedHole = false;
+                        gameEnded = false;
+                        restartClicked = true;
+                    }
+                }
+            }
         }
 
         if (!gameEnded) {
-                    if (ballReleased) {
-                        if (ballSprite.getPosition().y + 25 >= windowHeight) {
-                            if (!passedHole) {
-                                lives--;
+            if (ballReleased) {
+                if (ballSprite.getPosition().y + 25 >= windowHeight) {
+                    if (!passedHole) {
+                        lives--;
 
-                                blastPosition = ballSprite.getPosition();
+                        blastPosition = ballSprite.getPosition();
 
-                                blasting = true;
-                                blastFrame = 0;
-                                blastFrameCounter = 0;
-                            }
-                            ballReleased = false;
-                            passedHole = false;
-                            ballSprite.setPosition(rand() % (windowWidth - ballRadius * 2) + ballRadius, 10);
-                        }
-                        else {
-                            ballSprite.move(0, 5);
-                            if (!passedHole && ballSprite.getGlobalBounds().intersects(holeBar.getGlobalBounds())) {
-                                passedHole = true;
-                                holeSpeed *= speedIncrease; //Increase speed as score increases.
-                                score++;
-                            }
-                        }
+                        blasting = true;
+                        blastFrame = 0;
+                        blastFrameCounter = 0;
                     }
-//                    else {
-//                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && holeBar.getPosition().x + holeWidth < windowWidth) {
-//                            //ground.move(groundSpeed, 0);
-//                            holeBar.move(groundSpeed, 0);
-////                            blasting = true;
-//                        }
-//                        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && holeBar.getPosition().x > 0) {
-//                            //ground.move(-groundSpeed, 0);
-//                            holeBar.move(-groundSpeed, 0);
-//                        }
-//                    }
-
-                    // Move the hole bar automatically
-                    if (!ballReleased) {
-                        holeBar.move(holeSpeed, 0);
-
-                        // Check for collision with window edges and change direction
-                        if (holeBar.getPosition().x <= 0) {
-                            holeBar.setPosition(0, windowHeight - barHeight);
-                            holeSpeed = -holeSpeed;
-                        }
-                        else if (holeBar.getPosition().x + holeWidth >= windowWidth) {
-                            holeBar.setPosition(windowWidth - holeWidth, windowHeight - barHeight);
-                            holeSpeed = -holeSpeed;
-                        }
+                    ballReleased = false;
+                    passedHole = false;
+                    ballSprite.setPosition(rand() % (windowWidth - ballRadius * 2) + ballRadius, 10);
+                }
+                else {
+                    ballSprite.move(0, 5);
+                    if (!passedHole && ballSprite.getGlobalBounds().intersects(holeBar.getGlobalBounds())) {
+                        passedHole = true;
+                        holeSpeed *= speedIncrease; //Increase speed as score increases.
+                        score++;
                     }
-
-                    window.clear();
-
-                    // Draw background
-                    window.draw(backgroundSprite);
-
-                    // Draw ground
-                    window.draw(ground);
-
-                    // Draw hole bar
-                    window.draw(holeBar);
-
-                    // Draw ball sprite
-                    window.draw(ballSprite);
-
-                    if (blasting){
-                        blastFrameCounter++;
-                        if (blastFrameCounter >= blastFrameDuration){
-                            blastFrame++;
-                            blastFrameCounter = 0;
-                            if (blastFrame >= numBlastFrames){
-                                blasting = false;
-                                blastFrame = 0;
-                                }
-                        }
-                        blastSprite.setTextureRect(sf::IntRect(blastFrame * blastSprite.getTextureRect().width, 0, blastSprite.getTextureRect().width, blastSprite.getTextureRect().height));
-                        blastSprite.setPosition(blastPosition);
-                        window.draw(blastSprite);
-                    }
-
-                    scoreText.setString("Score: " + std::to_string(score));
-                    window.draw(scoreText);
-
-                    livesText.setString("Lives: " + std::to_string(lives));
-                    window.draw(livesText);
-
-
-                    if (lives <= 0) {
-                        gameEnded = true;
-                        gameOverText.setPosition((windowWidth - gameOverText.getLocalBounds().width) / 2, (windowHeight - gameOverText.getLocalBounds().height) / 2);
-                        gameOverText.setString("Game Over! \nYour Score is: " + std::to_string(score));
-                        window.draw(gameOverText);
-                    }
-                    window.display();
                 }
             }
 
-            return 0;
+            // Move the hole bar automatically
+            if (!ballReleased) {
+                holeBar.move(holeSpeed, 0);
+
+                // Check for collision with window edges and change direction
+                if (holeBar.getPosition().x <= 0) {
+                    holeBar.setPosition(0, windowHeight - barHeight);
+                    holeSpeed = -holeSpeed;
+                }
+                else if (holeBar.getPosition().x + holeWidth >= windowWidth) {
+                    holeBar.setPosition(windowWidth - holeWidth, windowHeight - barHeight);
+                    holeSpeed = -holeSpeed;
+                }
+            }
+
+            window.clear();
+
+            // Draw background
+            window.draw(backgroundSprite);
+
+            // Draw ground
+            window.draw(ground);
+
+            // Draw hole bar
+            window.draw(holeBar);
+
+            // Draw ball sprite
+            window.draw(ballSprite);
+
+            if (blasting) {
+                blastFrameCounter++;
+                if (blastFrameCounter >= blastFrameDuration) {
+                    blastFrame++;
+                    blastFrameCounter = 0;
+                    if (blastFrame >= numBlastFrames) {
+                        blasting = false;
+                        blastFrame = 0;
+                    }
+                }
+                blastSprite.setTextureRect(sf::IntRect(blastFrame * blastSprite.getTextureRect().width, 0, blastSprite.getTextureRect().width, blastSprite.getTextureRect().height));
+                blastSprite.setPosition(blastPosition);
+                window.draw(blastSprite);
+            }
+
+            scoreText.setString("Score: " + std::to_string(score));
+            window.draw(scoreText);
+
+            livesText.setString("Lives: " + std::to_string(lives));
+            window.draw(livesText);
+
+            if (lives <= 0) {
+                gameEnded = true;
+                restartButton.setPosition((windowWidth - restartButton.getLocalBounds().width) / 2, (windowHeight - restartButton.getLocalBounds().height) / 2 + 100);
+                gameOverText.setPosition((windowWidth - gameOverText.getLocalBounds().width) / 2, (windowHeight - gameOverText.getLocalBounds().height) / 2);
+                gameOverText.setString("Game Over! \nYour Score is: " + std::to_string(score));
+                window.draw(gameOverText);
+                window.draw(restartButton);
+            }
+
+            window.display();
         }
+
+        if (restartClicked) {
+            // Reset hole bar position
+            holeBar.setPosition((windowWidth - holeWidth) / 2, windowHeight - barHeight);
+
+            // Reset ball position
+            ballSprite.setPosition(rand() % (windowWidth - ballRadius * 2) + ballRadius + 150, 10);
+
+            // Reset game variables
+            score = 0;
+            lives = 3;
+            holeSpeed = 2.0f;
+            ballReleased = false;
+            passedHole = false;
+            gameEnded = false;
+            restartClicked = false;
+        }
+    }
+
+    return 0;
+}
